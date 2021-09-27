2a8ac647a394d69c5192c82d547214a555e033ed
colin <ttbuge@gmail.com>
2021-09-27 16:56:30 +0800

    update dapui's api.

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -222,6 +222,16 @@ function config.toggleterm()
 end
 
 function config.dapui()
+    local dap, dapui = require('dap'), require('dapui')
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+    end
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+    end
+    dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+    end
     require("dapui").setup({
         icons = {expanded = "▾", collapsed = "▸"},
         mappings = {
