2207cad68cee1b696a534657cbb358e6b58b5145
colin <ttbuge@gmail.com>
2021-11-08 15:50:44 +0800

    lualine and lsp-installer config

--- lua/modules/completion/lspconfig.lua
+++ lua/modules/completion/lspconfig.lua
@@ -6,8 +6,8 @@ if not packer_plugins['lspsaga.nvim'].loaded then
     vim.cmd [[packadd lspsaga.nvim]]
 end
 
-if not packer_plugins['nvim-lspinstall'].loaded then
-    vim.cmd [[packadd nvim-lspinstall]]
+if not packer_plugins['nvim-lsp-installer'].loaded then
+    vim.cmd [[packadd nvim-lsp-installer]]
 end
 
 if not packer_plugins['lsp_signature.nvim'].loaded then
@@ -15,7 +15,18 @@ if not packer_plugins['lsp_signature.nvim'].loaded then
 end
 
 local nvim_lsp = require('lspconfig')
-local lsp_install = require('lspinstall')
+-- local lsp_install = require('lspinstall')
+local lsp_installer = require('nvim-lsp-installer')
+
+lsp_installer.settings {
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+}
 -- local saga = require('lspsaga')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
@@ -38,65 +49,138 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
     properties = {'documentation', 'detail', 'additionalTextEdits'}
 }
 
-local function setup_servers()
-    lsp_install.setup()
-    local servers = lsp_install.installed_servers()
-    for _, lsp in pairs(servers) do
-        if lsp == "lua" then
-            nvim_lsp[lsp].setup {
-                capabilities = capabilities,
-                flags = {debounce_text_changes = 500},
-                settings = {
-                    Lua = {
-                        diagnostics = {globals = {"vim", "packer_plugins"}},
-                        workspace = {
-                            library = {
-                                [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                                [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true
-                            },
-                            maxPreload = 100000,
-                            preloadFileSize = 10000
-                        },
-                        telemetry = {enable = false}
-                    }
-                },
-                on_attach = function()
-                    require('lsp_signature').on_attach({
-                        bind = true,
-                        use_lspsaga = false,
-                        floating_window = true,
-                        fix_pos = true,
-                        hint_enable = true,
-                        hi_parameter = "Search",
-                        handler_opts = {"double"},
-                        zindex = 50,
-                        transpancy = 20
-                    })
-                end
-            }
-        else
-            nvim_lsp[lsp].setup {
-                capabilities = capabilities,
-                flags = {debounce_text_changes = 500},
-                on_attach = function()
-                    require('lsp_signature').on_attach({
-                        bind = true,
-                        use_lspsaga = false,
-                        floating_window = true,
-                        fix_pos = true,
-                        hint_enable = true,
-                        hi_parameter = "Search",
-                        handler_opts = {"double"}
-                    })
-                end
-            }
+-- local function setup_servers()
+--     lsp_install.setup()
+--     local servers = lsp_install.installed_servers()
+--     for _, lsp in pairs(servers) do
+--         if lsp == "lua" then
+--             nvim_lsp[lsp].setup {
+--                 capabilities = capabilities,
+--                 flags = {debounce_text_changes = 500},
+--                 settings = {
+--                     Lua = {
+--                         diagnostics = {globals = {"vim", "packer_plugins"}},
+--                         workspace = {
+--                             library = {
+--                                 [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+--                                 [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true
+--                             },
+--                             maxPreload = 100000,
+--                             preloadFileSize = 10000
+--                         },
+--                         telemetry = {enable = false}
+--                     }
+--                 },
+--                 on_attach = function()
+--                     require('lsp_signature').on_attach({
+--                         bind = true,
+--                         use_lspsaga = false,
+--                         floating_window = true,
+--                         fix_pos = true,
+--                         hint_enable = true,
+--                         hi_parameter = "Search",
+--                         handler_opts = {"double"},
+--                         zindex = 50,
+--                         transpancy = 20
+--                     })
+--                 end
+--             }
+--         else
+--             nvim_lsp[lsp].setup {
+--                 capabilities = capabilities,
+--                 flags = {debounce_text_changes = 500},
+--                 on_attach = function()
+--                     require('lsp_signature').on_attach({
+--                         bind = true,
+--                         use_lspsaga = false,
+--                         floating_window = true,
+--                         fix_pos = true,
+--                         hint_enable = true,
+--                         hi_parameter = "Search",
+--                         handler_opts = {"double"}
+--                     })
+--                 end
+--             }
+--         end
+--     end
+-- end
+
+-- lsp_install.post_install_hook = function()
+--     setup_servers() -- reload installed servers
+--     vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+-- end
+
+-- setup_servers()
+
+local function custom_attach()
+    require('lsp_signature').on_attach({
+        bind = true,
+        use_lspsaga = false,
+        floating_window = true,
+        fix_pos = true,
+        hint_enable = true,
+        hi_parameter = "Search",
+        handler_opts = {"double"}
+    })
+end
+
+local function switch_source_header_splitcmd(bufnr, splitcmd)
+    bufnr = nvim_lsp.util.validate_bufnr(bufnr)
+    local params = {uri = vim.uri_from_bufnr(bufnr)}
+    vim.lsp.buf_request(bufnr, 'textDocument/switchSourceHeader', params,
+                        function(err, _, result)
+        if err then error(tostring(err)) end
+        if not result then
+            print("Corresponding file can’t be determined")
+            return
         end
+        vim.api.nvim_command(splitcmd .. ' ' .. vim.uri_to_fname(result))
+    end)
+end
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+
+    if (server.name == "sumneko_lua") then
+        opts.settings = {
+            Lua = {
+                diagnostics = {globals = {"vim", "packer_plugins"}},
+                workspace = {
+                    library = {
+                        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true
+                    },
+                    maxPreload = 100000,
+                    preloadFileSize = 10000
+                },
+                telemetry = {enable = false}
+            }
+        }
+    elseif (server.name == "clangd") then
+        opts.commands = {
+            ClangdSwitchSourceHeader = {
+                function()
+                    switch_source_header_splitcmd(0, "edit")
+                end,
+                description = "Open source/header in current buffer"
+            },
+            ClangdSwitchSourceHeaderVSplit = {
+                function()
+                    switch_source_header_splitcmd(0, "vsplit")
+                end,
+                description = "Open source/header in a new vsplit"
+            },
+            ClangdSwitchSourceHeaderSplit = {
+                function()
+                    switch_source_header_splitcmd(0, "split")
+                end,
+                description = "Open source/header in a new split"
+            }
+        }
     end
-end
+    opts.capabilities = capabilities
+    opts.flags = {debounce_text_changes = 500}
+    opts.on_attach = custom_attach
 
-lsp_install.post_install_hook = function()
-    setup_servers() -- reload installed servers
-    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
-setup_servers()
+    server:setup(opts)
+end)
--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -9,36 +9,82 @@ function config.edge()
     vim.g.edge_better_performance = 1
 end
 
-function config.lualine()
-    local function lsp()
-        local icon = [[  LSP: ]]
-        local msg = 'No Active LSP'
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then return icon .. msg end
-        for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return icon .. client.name
-            end
-        end
-        return icon .. msg
-    end
+-- function config.lualine()
+--     local function lsp()
+--         local icon = [[  LSP: ]]
+--         local msg = 'No Active LSP'
+--         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+--         local clients = vim.lsp.get_active_clients()
+--         if next(clients) == nil then return icon .. msg end
+--         for _, client in ipairs(clients) do
+--             local filetypes = client.config.filetypes
+--             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+--                 return icon .. client.name
+--             end
+--         end
+--         return icon .. msg
+--     end
 
+--     local gps = require("nvim-gps")
+
+--     require('lualine').setup {
+--         options = {
+--             icons_enabled = true,
+--             theme = 'gruvbox',
+--             disabled_filetypes = {}
+--         },
+
+--         sections = {
+--             lualine_a = {'mode'},
+--             lualine_b = {{'branch'}, {'diff'}},
+--             lualine_c = {
+--                 {'filename'}, {gps.get_location, condition = gps.is_available}
+--             },
+--             lualine_x = {
+--                 {
+--                     'diagnostics',
+--                     sources = {'nvim_lsp'},
+--                     color_error = "#BF616A",
+--                     color_warn = "#EBCB8B",
+--                     color_info = "#81A1AC",
+--                     color_hint = "#88C0D0",
+--                     symbols = {error = ' ', warn = ' ', info = ' '}
+--                 },
+--             },
+--             lualine_y = {'filetype', 'encoding', 'fileformat'},
+--             lualine_z = {'progress', 'location'}
+--         },
+--         inactive_sections = {
+--             lualine_a = {},
+--             lualine_b = {},
+--             lualine_c = {'filename'},
+--             lualine_x = {'location'},
+--             lualine_y = {},
+--             lualine_z = {}
+--         },
+--         tabline = {},
+--         extensions = {}
+--     }
+-- end
+
+function config.lualine()
     local gps = require("nvim-gps")
 
     require('lualine').setup {
         options = {
             icons_enabled = true,
-            theme = 'gruvbox',
-            disabled_filetypes = {}
+            theme = 'onedark',
+            disabled_filetypes = {},
+            component_separators = '|',
+            section_separators = {left = '', right = ''}
         },
 
         sections = {
             lualine_a = {'mode'},
             lualine_b = {{'branch'}, {'diff'}},
             lualine_c = {
-                {'filename'}, {gps.get_location, condition = gps.is_available}
+                -- {'filename'}, {gps.get_location, condition = gps.is_available}, {'lsp_progress'}
+                {gps.get_location, condition = gps.is_available}, {'lsp_progress'}
             },
             lualine_x = {
                 {
