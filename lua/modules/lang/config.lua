1d6e8a36d1e471e0296075523eca816826b3dfb9
husheng <husheng@joyy.com>
2022-05-16 21:14:21 +0800

    配置优化

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -85,7 +85,11 @@ function config.rust_tools()
     require('rust-tools').setup(opts)
 end
 
-function config.lang_go() vim.g.go_doc_keywordprg_enabled = false end
+function config.lang_go() 
+    vim.g.go_doc_keywordprg_enabled = 0
+	vim.g.go_def_mapping_enabled = 0
+	vim.g.go_code_completion_enabled = 0
+end
 
 --function config.lang_org()
 --    require('orgmode').setup({
