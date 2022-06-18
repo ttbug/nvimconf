5ebee580c0ce9ed7e7677bd815eec27660958130
husheng <husheng@joyy.com>
2022-06-18 21:24:53 +0800

    修复一些废弃的配置

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -200,21 +200,27 @@ function config.dapui()
 			edit = "e",
 			repl = "r",
 		},
-		sidebar = {
-			elements = {
-				-- Provide as ID strings or tables with "id" and "size" keys
-				{
-					id = "scopes",
-					size = 0.25, -- Can be float or integer > 1
-				},
-				{ id = "breakpoints", size = 0.25 },
-				{ id = "stacks", size = 0.25 },
-				{ id = "watches", size = 00.25 },
-			},
-			size = 40,
-			position = "left",
+		layouts = {
+            {
+                elements = {
+			    	-- Provide as ID strings or tables with "id" and "size" keys
+			    	{
+			    		id = "scopes",
+			    		size = 0.25, -- Can be float or integer > 1
+			    	},
+			    	"breakpoints",
+			    	"stacks",
+			    	"watches",
+			    },
+			    size = 40,
+			    position = "left",
+            },
+		    { 
+                elements = { "repl", "console" }, 
+                size = 10, 
+                position = "bottom", 
+            },
 		},
-		tray = { elements = { "repl" }, size = 10, position = "bottom" },
 		floating = {
 			max_height = nil,
 			max_width = nil,
