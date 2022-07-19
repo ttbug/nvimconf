e0e2a40f1dc48cce3c171e6ae4c123787215ca78
husheng <husheng@joyy.com>
2022-07-19 21:30:19 +0800

    chore: 配置优化

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -498,6 +498,9 @@ function config.nvim_bufferline()
 					padding = 1,
 				},
 			},
+            diagnostics_indicator = function(count, level, diagnostics_dict, context)
+				return "(" .. count .. ")"
+			end,
 		},
 	})
 end
