ab62ee672feb725394c182309423c3ca99adc24d
husheng <husheng@joyy.com>
2023-02-15 19:35:31 +0800

    fix: wilder No candidates found

--- lua/modules/configs/tool/wilder.lua
+++ lua/modules/configs/tool/wilder.lua
@@ -5,25 +5,25 @@ return function()
 
 	wilder.setup({ modes = { ":", "/", "?" } })
 	wilder.set_option("use_python_remote_plugin", 0)
-	wilder.set_option("pipeline", {
-		wilder.branch(
-			wilder.cmdline_pipeline({ use_python = 0, fuzzy = 1, fuzzy_filter = wilder.lua_fzy_filter() }),
-			wilder.vim_search_pipeline(),
-			{
-				wilder.check(function(_, x)
-					return x == ""
-				end),
-				wilder.history(),
-				wilder.result({
-					draw = {
-						function(_, x)
-							return icons.ui.Calendar .. " " .. x
-						end,
-					},
-				}),
-			}
-		),
-	})
+	--wilder.set_option("pipeline", {
+	--	wilder.branch(
+	--		wilder.cmdline_pipeline({ use_python = 0, fuzzy = 1, fuzzy_filter = wilder.lua_fzy_filter() }),
+	--		wilder.vim_search_pipeline(),
+	--		{
+	--			wilder.check(function(_, x)
+	--				return x == ""
+	--			end),
+	--			wilder.history(),
+	--			wilder.result({
+	--				draw = {
+	--					function(_, x)
+	--						return icons.ui.Calendar .. " " .. x
+	--					end,
+	--				},
+	--			}),
+	--		}
+	--	),
+	--})
 
 	local match_hl = require("modules.utils").hl_to_rgb("String", false, colors.green)
 
