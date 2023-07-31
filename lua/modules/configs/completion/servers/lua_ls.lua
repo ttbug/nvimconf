987dd72c495c79d928a49ed84494ffbe403b79fe
husheng <husheng@joyy.com>
2023-07-31 18:13:28 +0800

    fix: dap

--- lua/modules/configs/completion/servers/lua_ls.lua
+++ lua/modules/configs/completion/servers/lua_ls.lua
@@ -1,23 +1,10 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/lua_ls.lua
 return {
+	cmd = { "lua-language-server" },
 	settings = {
 		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-				disable = { "different-requires" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				},
-				maxPreload = 100000,
-				preloadFileSize = 10000,
-			},
-			hint = { enable = true, setType = true },
+			-- keep it commneted until 0.10 got released
+			-- hint = { enable = true, setType = true },
 			format = { enable = false },
 			telemetry = { enable = false },
 			-- Do not override treesitter lua highlighting with lua_ls's highlighting
