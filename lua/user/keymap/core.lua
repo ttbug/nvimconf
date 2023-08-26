9f1270e0704690c97ad49536658aa3baf387b019
胡胜 <ttbug@hushengdeMacBook-Pro.local>
2023-08-26 21:11:17 +0800

    修改快捷键

--- lua/user/keymap/core.lua
+++ lua/user/keymap/core.lua
@@ -1,1 +1,7 @@
-return {}
+local bind = require("keymap.bind")
+local map_cr = bind.map_cr
+
+return {
+	["n|<C-l>"] = map_cr("BufferLineCycleNext"):with_noremap():with_silent(),
+	["n|<C-k>"] = map_cr("BufferLineCyclePrev"):with_noremap():with_silent(),
+}
--- lua/user/keymap/ui.lua
+++ lua/user/keymap/ui.lua
@@ -4,6 +4,4 @@ local map_cr = bind.map_cr
 return {
 	["n|<A-S-j>"] = false,
 	["n|<A-S-k>"] = false,
-	["n|<C-l>"] = map_cr("BufferLineMoveNext"):with_noremap():with_silent():with_desc("buffer: Move current to next"),
-	["n|<C-k>"] = map_cr("BufferLineMovePrev"):with_noremap():with_silent():with_desc("buffer: Move current to prev"),
 }
