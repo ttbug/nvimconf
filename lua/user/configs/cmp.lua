b529b43f063d8c5e5e4b9a653d0e171b26d3f153
husheng <husheng@joyy.com>
2023-09-06 17:38:31 +0800

    add codeium plugin

---
+++ lua/user/configs/cmp.lua
@@ -0,0 +1,5 @@
+return {
+	sources = {
+		{ name = "codeium" },
+	},
+}
---
+++ lua/user/plugins/completion.lua
@@ -0,0 +1,13 @@
+local custom = {}
+
+custom["jcdickinson/codeium.nvim"] = {
+	lazy = true,
+	event = { "CmdlineEnter" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = require("completion.codeium"),
+}
+
+return custom
