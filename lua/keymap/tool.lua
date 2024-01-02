4ec9aedaa93c268d8200a4ccc8c9efff77a68824
Mohu <lgt986452565@gmail.com>
2024-01-02 10:22:52 +0800

    fix: input delay in terminal mode (#1119)
    See: https://github.com/akinsho/toggleterm.nvim/issues/63

--- lua/keymap/tool.lua
+++ lua/keymap/tool.lua
@@ -23,7 +23,7 @@ local plug_map = {
 	-- Plugin: toggleterm
 	["t|<Esc><Esc>"] = map_cmd([[<C-\><C-n>]]):with_noremap():with_silent(), -- switch to normal mode in terminal.
 	["t|jk"] = map_cmd([[<C-\><C-n>]]):with_noremap():with_silent(), -- switch to normal mode in terminal.
-	["n|<C-\\>"] = map_cr([[execute v:count . "ToggleTerm direction=horizontal"]])
+	["n|<C-\\>"] = map_cr("ToggleTerm direction=horizontal")
 		:with_noremap()
 		:with_silent()
 		:with_desc("terminal: Toggle horizontal"),
