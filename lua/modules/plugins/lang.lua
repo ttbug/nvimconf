d0ed4b51039581120c3e9fe454fa8ef5187ca43e
husheng <husheng@joyy.com>
2024-04-19 11:12:26 +0800

    优化Go开发配置

--- lua/modules/plugins/lang.lua
+++ lua/modules/plugins/lang.lua
@@ -23,16 +23,16 @@ lang["ray-x/go.nvim"] = {
 		--"neovim/nvim-lspconfig",
 		--"nvim-treesitter/nvim-treesitter",
 	},
-	ft = { "go", "gomod" },
+	ft = { "go", "gomod", "gosum" },
 	-- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	build = ":GoInstallBinaries",
-	config = require("lang.go-nvim"),
+	config = require("lang.go"),
 }
 lang["mrcjkb/rustaceanvim"] = {
 	lazy = true,
 	ft = "rust",
 	version = "^3",
-	config = require("lang.rust"),
+	init = require("lang.rust"),
 	dependencies = { "nvim-lua/plenary.nvim" },
 }
 lang["Saecki/crates.nvim"] = {
