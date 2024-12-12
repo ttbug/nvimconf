821fafdfb960fe6772d25ba50ee8f2ef6dd21e43
husheng <husheng@joyy.com>
2024-12-12 15:23:40 +0800

    fix: typo

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -1,6 +1,13 @@
 local custom = {}
 custom["folke/tokyonight.nvim"] = {
 	lazy = false,
+	opts = {
+		transparent = false,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
 }
 
 return custom
